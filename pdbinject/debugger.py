c5c8ab911bb75d8a6aa277ce2fe188af143c5b76
David Cramer <dcramer@gmail.com>
2013-06-19 12:51:55 -0700

    Correct rpdb args

--- pdbinject/debugger.py
+++ pdbinject/debugger.py
@@ -12,11 +12,11 @@ class DebuggerThread(Thread):
 
 
 class RemoteDebuggerThread(Thread):
-    def __init__(self, address='127.0.0.1', port=4444):
-        self.address = address
+    def __init__(self, addr='127.0.0.1', port=4444):
+        self.addr = addr
         self.port = port
         Thread.__init__(self)
 
     def run(self):
-        debugger = rpdb.Rpdb(address=self.address, port=self.port)
+        debugger = rpdb.Rpdb(addr=self.address, port=self.port)
         debugger.set_trace()
--- pdbinject/runner.py
+++ pdbinject/runner.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 
 
-def inject(pid, verbose=False, gdb_prefix='', rpdb_address='127.0.0.1',
+def inject(pid, verbose=False, gdb_prefix='', rpdb_addr='127.0.0.1',
            rpdb_port=4444):
     """Executes a file in a running Python process."""
     # TODO: rpdb stuff
@@ -13,12 +13,12 @@ def inject(pid, verbose=False, gdb_prefix='', rpdb_address='127.0.0.1',
         'PyRun_SimpleString("'
             'import sys; sys.path.insert(0, \\"%(path)s\\");'
             'from pdbinject.debugger import RemoteDebuggerThread;'
-            'thread = RemoteDebuggerThread(address=\\"%(rpdb_address)s\\", port=%(rdpb_port)d);'
+            'thread = RemoteDebuggerThread(addr=\\"%(rpdb_addr)s\\", port=%(rdpb_port)d);'
             'thread.start();'
             'time.sleep(1)'
         '")' % dict(
             path=os.path.join(os.path.dirname(__file__), os.pardir),
-            rpdb_address=rpdb_address,
+            rpdb_addr=rpdb_addr,
             rdpb_port=rpdb_port,
         ),
         'PyGILState_Release($1)',
