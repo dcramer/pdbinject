d5697d749b7697b69bb1098ca29e6ca9eabcd5c9
David Cramer <dcramer@gmail.com>
2015-03-03 23:48:11 -0800

    0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pdbinject',
-    version='0.1.0',
+    version='0.2.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/pdbinject',
